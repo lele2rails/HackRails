1b8a4db62616e189bfbbe6a8b58b321232ae7e24
lele <invisible.lele@gmail.com>
2011-04-03 16:55:51 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackRails::Application.config.secret_token = '694e092b2165e2ff0a7923a5c99c5ff37ab94c7449c603e94a822fffdc06eefcd3e019ab446ba0b30fa8afa983b42494852f771d70bafd8fb5ebf714af1c90ea'
